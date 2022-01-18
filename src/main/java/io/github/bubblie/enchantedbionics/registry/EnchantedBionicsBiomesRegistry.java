8cc5cfd59f1f274e29c9db2a44615281745e95d4
NoComment1105 <nocomment1105@outlook.com>
2022-01-18 07:29:22 +0000

    change: Make mystic jasmine less common and spawn in correct biomes

--- src/main/java/io/github/bubblie/enchantedbionics/registry/EnchantedBionicsBiomesRegistry.java
+++ src/main/java/io/github/bubblie/enchantedbionics/registry/EnchantedBionicsBiomesRegistry.java
@@ -4,12 +4,13 @@ import io.github.bubblie.enchantedbionics.world.gen.feature.EnchantedBionicsVege
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
 
 public class EnchantedBionicsBiomesRegistry {
 
 	public static void init() {
-		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.VEGETAL_DECORATION, BuiltinRegistries.PLACED_FEATURE.getKey(EnchantedBionicsVegetationPlacedFeatures.PATCH_MYSTIC_JASMINE).orElseThrow());
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FLOWER_FOREST, BiomeKeys.DARK_FOREST), GenerationStep.Feature.VEGETAL_DECORATION, BuiltinRegistries.PLACED_FEATURE.getKey(EnchantedBionicsVegetationPlacedFeatures.PATCH_MYSTIC_JASMINE).orElseThrow());
 	}
 
 }
--- src/main/java/io/github/bubblie/enchantedbionics/world/gen/feature/EnchantedBionicsVegetationPlacedFeatures.java
+++ src/main/java/io/github/bubblie/enchantedbionics/world/gen/feature/EnchantedBionicsVegetationPlacedFeatures.java
@@ -3,7 +3,6 @@ package io.github.bubblie.enchantedbionics.world.gen.feature;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.decorator.BiomePlacementModifier;
-import net.minecraft.world.gen.decorator.CountPlacementModifier;
 import net.minecraft.world.gen.decorator.InSquarePlacementModifier;
 import net.minecraft.world.gen.decorator.RarityFilterPlacementModifier;
 import net.minecraft.world.gen.feature.PlacedFeature;
@@ -13,8 +12,7 @@ public class EnchantedBionicsVegetationPlacedFeatures {
 
 	public static final PlacedFeature PATCH_MYSTIC_JASMINE = register("patch_mystic_jasmine",
 			EnchantedBionicsVegetationConfiguredFeatures.PATCH_MYSTIC_JASMINE.withPlacement(
-					CountPlacementModifier.create(3),
-					RarityFilterPlacementModifier.create(2),
+					RarityFilterPlacementModifier.create(36),
 					InSquarePlacementModifier.getInstance(),
 					PlacedFeatureUtil.MOTION_BLOCKING_HEIGHTMAP,
 					BiomePlacementModifier.getInstance()
